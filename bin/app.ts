675adf08be8ac5fb0d6209b62216212a929b4839
Bogdan Popa <bogdan@kreatorhood.com>
2024-07-01 16:01:34 +0300

    chore: add allow headers

--- bin/app.ts
+++ bin/app.ts
@@ -349,6 +349,7 @@ const jsonRpcProviders = {
   WEB3_RPC_8453: process.env.WEB3_RPC_8453!,
   WEB3_RPC_324: process.env.WEB3_RPC_324!,
   WEB3_RPC_842: process.env.WEB3_RPC_842!,
+  WEB3_RPC_841: process.env.WEB3_RPC_841!,
   // The followings are for RPC Gateway
   // Optimism
   // INFURA_10: process.env.INFURA_10!,
